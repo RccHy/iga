6d014e1af54262e8bffdce234881807cef64420a
任宸赐 <ccren@quantangle.com.cn>
2021-03-12 15:07:46 +0800

    20210312

---
+++ src/main/java/com/qtgl/iga/bean/DeptBean.java
@@ -0,0 +1,14 @@
+package com.qtgl.iga.bean;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DeptBean {
+
+    private String code;
+    private String name;
+    private String parentCode;
+}
