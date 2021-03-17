dc60d1ede75fce9152f3ac5a909f7332962e09bf
任宸赐 <ccren@quantangle.com.cn>
2021-03-17 11:21:23 +0800

    20210315

--- src/main/java/com/qtgl/iga/dao/impl/DomainInfoDaoImpl.java
+++ src/main/java/com/qtgl/iga/dao/impl/DomainInfoDaoImpl.java
@@ -25,16 +25,16 @@ public class DomainInfoDaoImpl implements DomainInfoDao {
     public void save(DomainInfo domainInfo) {
         jdbcIGA.update("INSERT INTO `t_mgr_domain_info`(`id`, `domain_id`," +
                         " `domain_name` ,`client_id` , `client_secret` , " +
-                        "`status`, `create_time` , `update_time` ) " +
+                        "`status`, `create_time`,`create_user` ,`update_time` ) " +
                         "VALUES (?, ?, ?, ?, ?, ?, ?, ?);",
-                domainInfo.getId(), domainInfo.getDomainId(), domainInfo.getDomainName(), domainInfo.getClientId(), domainInfo.getClientSecret(), domainInfo.getStatus(), domainInfo.getCreateTime(), domainInfo.getUpdateTime()
+                domainInfo.getId(), domainInfo.getDomainId(), domainInfo.getDomainName(), domainInfo.getClientId(), domainInfo.getClientSecret(), domainInfo.getStatus(), domainInfo.getCreateTime(), domainInfo.getCreateUser(),domainInfo.getUpdateTime()
         );
     }
 
     @Override
     public List<DomainInfo> findAll() {
         List<Map<String, Object>> mapList = jdbcIGA.queryForList("select id,domain_id as domainId,domain_name as domainName,client_id as clientId,client_secret as clientSecret," +
-                "create_time as createTime,create_user as createUser,status from t_mgr_domain_info where status=0");
+                "create_time as createTime,create_user as createUser,status,update_time as updateTime from t_mgr_domain_info where status=0");
         ArrayList<DomainInfo> list = new ArrayList<>();
         if (null != mapList && mapList.size() > 0) {
             for (Map<String, Object> map : mapList) {
@@ -57,7 +57,7 @@ public class DomainInfoDaoImpl implements DomainInfoDao {
 
     @Override
     public DomainInfo getByDomainName(String name) {
-        List<Map<String, Object>> mapList = jdbcIGA.queryForList("select id,domain_id as domainId,domain_name as domainName,client_id as clientId,client_secret as clientSecret,create_time as createTime,create_user as createUser,status from t_mgr_domain_info where status=0");
+        List<Map<String, Object>> mapList = jdbcIGA.queryForList("select id,domain_id as domainId,domain_name as domainName,client_id as clientId,client_secret as clientSecret,create_time as createTime,update_time as updateTime,create_user as createUser,status from t_mgr_domain_info where status=0");
         DomainInfo domainInfo = new DomainInfo();
         if (null != mapList && mapList.size() > 0) {
             for (Map<String, Object> map : mapList) {
