28dcdcf236897e3353c72cbfce0053c2b68423d6
任宸赐 <ccren@quantangle.com.cn>
2021-03-23 15:17:32 +0800

    20210323

--- src/main/java/com/qtgl/iga/bean/DeptBean.java
+++ src/main/java/com/qtgl/iga/bean/DeptBean.java
@@ -15,7 +15,20 @@ public class DeptBean {
 
     private String parentCode;
 
+    private Boolean independent;
+
+    private String tags;
+
+    private String description;
+
+    private String meta;
+
     private String source;
 
+    // push  pull  builtin
+    private String dataSource;
+
     private Timestamp createTime;
+
+
 }
