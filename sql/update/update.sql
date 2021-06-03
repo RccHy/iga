87830ce0ebdc6182dd2e49ba66bde13a663f9702
任宸赐 <ccren@quantangle.com.cn>
2021-06-03 12:01:50 +0800

    20210603

--- sql/update/update.sql
+++ sql/update/update.sql
@@ -1,11 +1,26 @@
 -- 20210510
+create table t_mgr_task_log
+(
+    id          varchar(50) not null
+        primary key,
+    status      int         not null,
+    dept_no     int         null,
+    post_no     int         null,
+    person_no   int         null,
+    occupy_no   int         null,
+    create_time timestamp   null,
+    update_time timestamp   null,
+    domain varchar(50)   null comment '租户外键'
+)
+    comment '同步任务日志表';
+
 alter table t_mgr_task_log
 	add domain varchar(50) null comment '租户';
 alter table t_mgr_task_log modify status varchar(50) not null;
 
 
 
--- XXXXX
+-- 20210603
 alter table t_mgr_task_log modify dept_no varchar(50) null;
 alter table t_mgr_task_log modify post_no varchar(50) null;
 alter table t_mgr_task_log modify person_no varchar(50) null;
