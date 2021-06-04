7d531a567ec5fac6222172a3a55d1740457c136a
任宸赐 <ccren@quantangle.com.cn>
2021-06-04 15:06:02 +0800

    Update update.sql

--- sql/update/update.sql
+++ sql/update/update.sql
@@ -20,7 +20,7 @@ alter table t_mgr_task_log modify status varchar(50) not null;
 
 
 
--- 20210603
+-- 20210604
 alter table t_mgr_task_log modify dept_no varchar(50) null;
 alter table t_mgr_task_log modify post_no varchar(50) null;
 alter table t_mgr_task_log modify person_no varchar(50) null;
